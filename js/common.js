16759a90f5d03dc28126c782cee13912eb7b99fe
Viktoria Zaharova <zaharova181114@gmail.com>
2024-12-28 00:56:02 +0200

    pc version

--- js/common.js
+++ js/common.js
@@ -1,54 +1,64 @@
 // animate scroll
 $('.go_to').click(function (e) {
-  e.preventDefault();
-  var scroll_el = $(this).attr('href');
-  if ($(scroll_el).length !== 0) {
-      $('html, body').animate({
-          scrollTop: $(scroll_el).offset().top
-      }, 500);
-  }
-  return false;
+    e.preventDefault();
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
 });
 
 
 // модальные окна (несколько)
 $(function () {
-  let overlay = $('.overlay'),    
-      open_modal = $('.open_modal'),
-      close = $('.modal__close, .overlay'),
-      modal = $('.modal__div');
+    let overlay = $('.overlay'),
+        open_modal = $('.open_modal'),
+        close = $('.modal__close, .overlay'),
+        modal = $('.modal__div');
 
-  open_modal.on('click',function (event) {
-      event.preventDefault();
+    open_modal.on('click', function (event) {
+        event.preventDefault();
 
-      modal.css('display', 'none').animate({
-          opacity: 0,
-          top: '45%'
-      }, 200);
+        modal.css('display', 'none').animate({
+            opacity: 0,
+            top: '45%'
+        }, 200);
 
-      let div = $(this).attr('href');
-      overlay.fadeIn(400,
-          function () {
-              $(div)
-                  .css('display', 'flex')
-                  .animate({
-                      opacity: 1,
-                      top: '50%'
-                  }, 200);
-          });
-  });
+        let div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
 
-  close.on('click', function () {
-      modal
-          .animate({
-                  opacity: 0,
-                  top: '45%'
-              }, 200,
-              function () {
-                  $(this).css('display', 'none');
-                  overlay.fadeOut(400);
-              }
-          );
-  });
+    close.on('click', function () {
+        modal
+            .animate({
+                opacity: 0,
+                top: '45%'
+            }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
 });
 //end
+
+// tabs
+$(function () {
+
+    $('ul.tabs__caption').on('click', 'li.tabs__caption-click:not(.active)', function () {
+        $(this).addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+});
